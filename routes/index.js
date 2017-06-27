26d7a68dccd866baa359aef3fe65326f7d5a4d5a
mbwhite <matthew@mh-white.com>
2017-06-27 22:49:49 +0100

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var RequestClient = require("reqclient").RequestClient;
 
+var client = new RequestClient("http://localhost:3000/api/");
 var getImgs = require('./util.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var data = make rest call here;
+  var data = client.get({"uri": "available"});
   //getImgs();
+  console.log(data);
   data.title ='Express';
   res.render('cards', data);
 });
